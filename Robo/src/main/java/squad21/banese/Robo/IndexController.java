2e3ac3ca0119c2c4c6d9a4ef5d261158096d64da
Lz Gustavo <131289697+PlayerLzz@users.noreply.github.com>
2024-05-21 11:47:57 -0300

    Update IndexController.java

--- Robo/src/main/java/squad21/banese/Robo/IndexController.java
+++ Robo/src/main/java/squad21/banese/Robo/IndexController.java
@@ -1,19 +1,57 @@
 package squad21.banese.Robo;
+import java.time.LocalDateTime;
+
+public class Agendamento {
+    private String nomeCliente;
+    private LocalDateTime dataAgendamento;
+    private String tipoTarefa;
+
+	 public String getNomeCliente() {
+            return nomeCliente;
+        }
+
+        public void setNomeCliente(String nomeCliente) {
+            this.nomeCliente = nomeCliente;
+        }
+
+        public LocalDateTime getDataAgendamento() {
+            return dataAgendamento;
+        }
+
+        public void setDataAgendamento(LocalDateTime dataAgendamento) {
+            this.dataAgendamento = dataAgendamento;
+        }
+
+        public String getTipoTarefa() {
+            return tipoTarefa;
+        }
+
+        public void setTipoTarefa(String tipoTarefa) {
+            this.tipoTarefa = tipoTarefa;
+        }
+}
+
 
 @Controller
 public class IndexController {
 
-	@PostMapping("/registro/1")
-	public Robo registrarRobo() {
-		return registrarRobo();
-	}
+
+	@PostMapping("/agendamento")
+public ResponseEntity agendarRobo(@RequestBody Agendamento agendamento) {
+
+      return ResponseEntity.ok(HttpStatus.OK);
+}
+
+	
+	@PostMapping("/registro")
+public ResponseEntity registrarRobo(@RequestBody Robo robo) {
+
+      return ResponseEntity.ok(HttpStatus.OK);
+}
 
 	@GetMapping("/consulta/1")
 	public Robo consultarStatus() {
 		return consultarStatus();
 	}
 
-	@PostMapping("/agendamento/1")
-	public Robo agendarRobo() {
-		return agendarRobo();
-	}
+	
